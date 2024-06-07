ee9d8fa414fda28d533f190a6f565768cea47edd
Aldiantoro Nugroho <kriwil@gmail.com>
2024-06-07 21:39:13 +0700

    update wezterm, light mode and use fancy tabs

--- wezterm.lua
+++ wezterm.lua
@@ -1,91 +1,67 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
--- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+-- This will hold the configuration.
+local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = 'catppuccin-latte'
-config.use_fancy_tab_bar = false
+config.color_scheme = "PaperColor Light (base16)"
+config.font = wezterm.font("EnvyCodeR Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.font_size = 12.
+config.use_fancy_tab_bar = true
+
+config.window_frame = {
+	-- font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+	font = wezterm.font("Helvetica Neue", { weight = "Bold", stretch = "Condensed", style = "Normal" }),
+
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 12.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "rgba(202 202 203 1)",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "rgba(202 202 203 1)",
+}
 
 config.colors = {
 	tab_bar = {
-		-- The color of the strip that goes along the top of the window
-		-- (does not apply when fancy tab bar is in use)
-		background = '#e0e0e0',
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "rgba(202 202 203 1)",
 
 		-- The active tab is the one that has focus in the window
 		active_tab = {
-			-- The color of the background area for the tab
-			bg_color = '#e4e4e4',
-			-- The color of the text for the tab
-			fg_color = '#474747',
-
-			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-			-- label shown for this tab.
-			-- The default is "Normal"
-			intensity = 'Normal',
-
-			-- Specify whether you want "None", "Single" or "Double" underline for
-			-- label shown for this tab.
-			-- The default is "None"
-			underline = 'None',
-
-			-- Specify whether you want the text to be italic (true) or not (false)
-			-- for this tab.  The default is false.
-			italic = false,
-
-			-- Specify whether you want the text to be rendered with strikethrough (true)
-			-- or not for this tab.  The default is false.
-			strikethrough = false,
+			bg_color = "rgba(228 228 229 1)",
+			fg_color = "#404040",
 		},
 
 		-- Inactive tabs are the tabs that do not have focus
 		inactive_tab = {
-			bg_color = '#e0e0e0',
-			fg_color = '#979797',
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab`.
+			bg_color = "rgba(202 202 203 1)",
+			fg_color = "#808080",
 		},
 
 		-- You can configure some alternate styling when the mouse pointer
 		-- moves over inactive tabs
 		inactive_tab_hover = {
-			bg_color = '#3b3052',
-			fg_color = '#909090',
-			italic = true,
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab_hover`.
+			bg_color = "rgba(228 228 229 1)",
+			fg_color = "#808080",
 		},
 
 		-- The new tab button that let you create new tabs
 		new_tab = {
-			bg_color = '#e0e0e0',
-			fg_color = '#979797',
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab`.
+			bg_color = "rgba(202 202 203 1)",
+			fg_color = "#808080",
 		},
 
 		-- You can configure some alternate styling when the mouse pointer
 		-- moves over the new tab button
 		new_tab_hover = {
-			bg_color = '#3b3052',
-			fg_color = '#909090',
-			italic = true,
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab_hover`.
+			bg_color = "rgba(228 228 229 1)",
+			fg_color = "#808080",
 		},
 	},
 }
