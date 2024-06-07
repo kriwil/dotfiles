87a1792a3d1237396df9a2672f874eef55a3728e
Aldiantoro Nugroho <kriwil@gmail.com>
2024-06-08 00:55:45 +0700

    new wezterm config with custom keybinds

--- wezterm.lua
+++ wezterm.lua
@@ -1,64 +1,46 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
+local act = wezterm.action
 
 config.color_scheme = "PaperColor Light (base16)"
+
 config.font = wezterm.font("EnvyCodeR Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font_size = 12.
-config.use_fancy_tab_bar = true
 
 config.window_frame = {
-	-- font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-	font = wezterm.font("Helvetica Neue", { weight = "Bold", stretch = "Condensed", style = "Normal" }),
-
-	-- Default to 10.0 on Windows but 12.0 on other systems
+	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
 	font_size = 12.0,
 
-	-- The overall background color of the tab bar when
-	-- the window is focused
 	active_titlebar_bg = "rgba(202 202 203 1)",
-
-	-- The overall background color of the tab bar when
-	-- the window is not focused
 	inactive_titlebar_bg = "rgba(202 202 203 1)",
 }
 
 config.colors = {
 	tab_bar = {
-		-- The color of the inactive tab bar edge/divider
 		inactive_tab_edge = "rgba(202 202 203 1)",
 
-		-- The active tab is the one that has focus in the window
 		active_tab = {
 			bg_color = "rgba(228 228 229 1)",
 			fg_color = "#404040",
 		},
 
-		-- Inactive tabs are the tabs that do not have focus
 		inactive_tab = {
 			bg_color = "rgba(202 202 203 1)",
 			fg_color = "#808080",
 		},
 
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over inactive tabs
 		inactive_tab_hover = {
 			bg_color = "rgba(228 228 229 1)",
 			fg_color = "#808080",
 		},
 
-		-- The new tab button that let you create new tabs
 		new_tab = {
 			bg_color = "rgba(202 202 203 1)",
 			fg_color = "#808080",
 		},
 
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over the new tab button
 		new_tab_hover = {
 			bg_color = "rgba(228 228 229 1)",
 			fg_color = "#808080",
@@ -66,5 +48,44 @@ config.colors = {
 	},
 }
 
--- and finally, return the configuration to wezterm
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
+config.use_dead_keys = false
+config.scrollback_lines = 5000
+
+config.disable_default_key_bindings = true
+config.keys = {
+	{ key = "m", mods = "SUPER", action = act.Hide },
+	{ key = "h", mods = "SUPER", action = act.HideApplication },
+	{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
+	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+	{ key = "Copy", action = act.CopyTo("Clipboard") },
+	{ key = "Paste", action = act.PasteFrom("Clipboard") },
+
+	{ key = "r", mods = "SUPER|SHIFT", action = act.ReloadConfiguration },
+
+	{ key = "t", mods = "SUPER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "w", mods = "SUPER", action = act.CloseCurrentTab({ confirm = true }) },
+	{ key = "LeftArrow", mods = "SUPER", action = act.ActivateTabRelative(-1) },
+	{ key = "RightArrow", mods = "SUPER", action = act.ActivateTabRelative(1) },
+	{ key = "1", mods = "SUPER", action = act.ActivateTab(0) },
+	{ key = "2", mods = "SUPER", action = act.ActivateTab(1) },
+	{ key = "3", mods = "SUPER", action = act.ActivateTab(2) },
+	{ key = "4", mods = "SUPER", action = act.ActivateTab(3) },
+	{ key = "5", mods = "SUPER", action = act.ActivateTab(4) },
+	{ key = "6", mods = "SUPER", action = act.ActivateTab(5) },
+	{ key = "7", mods = "SUPER", action = act.ActivateTab(6) },
+	{ key = "8", mods = "SUPER", action = act.ActivateTab(7) },
+	{ key = "9", mods = "SUPER", action = act.ActivateTab(8) },
+	{ key = "0", mods = "SUPER", action = act.ActivateTab(-1) },
+
+	{ key = "v", mods = "SUPER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "SUPER|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "LeftArrow", mods = "SUPER", action = act.ActivatePaneDirection("Left") },
+	{ key = "RightArrow", mods = "SUPER", action = act.ActivatePaneDirection("Right") },
+	{ key = "UpArrow", mods = "SUPER", action = act.ActivatePaneDirection("Up") },
+	{ key = "DownArrow", mods = "SUPER", action = act.ActivatePaneDirection("Down") },
+}
+
 return config
