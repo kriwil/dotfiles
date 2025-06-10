67e787468e047ee7d9efb4d37142ba902ce8c22f
Aldiantoro Nugroho <kriwil@gmail.com>
2025-06-10 13:55:45 +0700

    dark mode

--- wezterm.lua
+++ wezterm.lua
@@ -11,7 +11,7 @@ wezterm.on("gui-startup", function()
 end)
 
 -- config.color_scheme = "Catppuccin Latte"
-config.color_scheme = "zenbones"
+config.color_scheme = "zenbones_dark"
 -- config.color_scheme = "zenwritten_light"
 
 config.font = wezterm.font("EnvyCodeR Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
@@ -21,40 +21,40 @@ config.window_frame = {
 	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
 	font_size = 13.0,
 
-	active_titlebar_bg = "rgba(202 202 203 1)",
-	inactive_titlebar_bg = "rgba(202 202 203 1)",
+	-- active_titlebar_bg = "rgba(202 202 203 1)",
+	-- inactive_titlebar_bg = "rgba(202 202 203 1)",
 }
 
-config.colors = {
-	tab_bar = {
-		inactive_tab_edge = "rgba(202 202 203 1)",
-
-		active_tab = {
-			bg_color = "rgba(228 228 229 1)",
-			fg_color = "#404040",
-		},
-
-		inactive_tab = {
-			bg_color = "rgba(202 202 203 1)",
-			fg_color = "#808080",
-		},
-
-		inactive_tab_hover = {
-			bg_color = "rgba(228 228 229 1)",
-			fg_color = "#808080",
-		},
-
-		new_tab = {
-			bg_color = "rgba(202 202 203 1)",
-			fg_color = "#808080",
-		},
-
-		new_tab_hover = {
-			bg_color = "rgba(228 228 229 1)",
-			fg_color = "#808080",
-		},
-	},
-}
+-- config.colors = {
+-- 	tab_bar = {
+-- 		inactive_tab_edge = "rgba(202 202 203 1)",
+--
+-- 		active_tab = {
+-- 			bg_color = "rgba(228 228 229 1)",
+-- 			fg_color = "#404040",
+-- 		},
+--
+-- 		inactive_tab = {
+-- 			bg_color = "rgba(202 202 203 1)",
+-- 			fg_color = "#808080",
+-- 		},
+--
+-- 		inactive_tab_hover = {
+-- 			bg_color = "rgba(228 228 229 1)",
+-- 			fg_color = "#808080",
+-- 		},
+--
+-- 		new_tab = {
+-- 			bg_color = "rgba(202 202 203 1)",
+-- 			fg_color = "#808080",
+-- 		},
+--
+-- 		new_tab_hover = {
+-- 			bg_color = "rgba(228 228 229 1)",
+-- 			fg_color = "#808080",
+-- 		},
+-- 	},
+-- }
 
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
