ad24f2500576897a0d0f29d5d05a0932fc56cd87
Aldiantoro Nugroho <kriwil@gmail.com>
2025-06-23 05:47:37 +0700

    wezterm ubuntu update

--- wezterm.ubuntu.lua
+++ wezterm.ubuntu.lua
@@ -10,7 +10,7 @@ wezterm.on("gui-startup", function()
 	window:gui_window():maximize()
 end)
 
-config.color_scheme = "zenbones"
+config.color_scheme = "zenbones_dark"
 
 config.font = wezterm.font("EnvyCodeR Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font_size = 11.
