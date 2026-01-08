c0923df1d1aaf77bd85aaf37c1822e76292fc2a6
Aldiantoro Nugroho <kriwil@gmail.com>
2026-01-08 23:14:49 +0700

    neovim: rename core.lua to colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,20 @@ return {
   {
     "zenbones-theme/zenbones.nvim",
     dependencies = { "rktjmp/lush.nvim", lazy = false, priority = 1000 },
+    enabled = false,
+    lazy = false,
+    priority = 1000,
   },
   {
     "tinted-theming/tinted-vim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
   },
   {
     "oskarnurm/koda.nvim",
+    lazy = false,
+    priority = 1000,
   },
   {
     "LazyVim/LazyVim",
