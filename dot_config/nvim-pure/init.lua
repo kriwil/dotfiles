552255c6d87d435c646e9fc34899bc5020be233a
Aldiantoro Nugroho <kriwil@gmail.com>
2026-01-09 20:30:12 +0700

    neovim: single file config

--- dot_config/nvim-pure/init.lua
+++ dot_config/nvim-pure/init.lua
@@ -1,13 +1,247 @@
 -- Some configs are taken from https://github.com/nvim-lua/kickstart.nvim
 
-require("config.globals")
-require("config.options")
-require("config.keymap")
-require("config.autocmd")
-require("plugins.base")
-require("plugins.mini")
-require("plugins.others")
-require("plugins.treesitter")
-require("plugins.lspconfig")
-require("plugins.formatter")
-require("plugins.finder")
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+
+local g = vim.g
+g.mapleader = " "
+g.maplocalleader = " "
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+local o = vim.o
+
+-- Make line numbers default
+o.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+o.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+o.mouse = "a"
+
+-- Don't show the mode, since it's already in the status line
+o.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  o.clipboard = "unnamedplus"
+end)
+
+-- Enable break indent
+o.breakindent = true
+
+-- Save undo history
+o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+o.ignorecase = true
+o.smartcase = true
+
+-- Keep signcolumn on by default
+o.signcolumn = "yes"
+
+-- Decrease update time
+o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+o.splitright = true
+o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Convert tab to space
+o.expandtab = true
+
+-- Preview substitutions live, as you type!
+o.inccommand = "split"
+
+-- Show which line your cursor is on
+o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+
+o.confirm = true
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+local map = vim.keymap.set
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+map("n", "<C-/>", "<cmd>terminal<CR>i", { desc = "Open terminal" })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+vim.pack.add({
+  "https://github.com/nvim-tree/nvim-web-devicons",
+  "https://github.com/rktjmp/lush.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
+})
+
+vim.pack.add({
+  "https://github.com/nvim-mini/mini.nvim",
+})
+
+require("mini.diff").setup({ view = { style = "sign" } })
+require("mini.files").setup()
+require("mini.pairs").setup()
+-- require("mini.pick").setup()
+require("mini.surround").setup()
+require("mini.statusline").setup()
+
+-- mini.files
+vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", { desc = "Open MiniFiles" })
+
+-- mini.pick
+-- vim.keymap.set("n", "<leader><leader>", "<cmd>Pick files<CR>", { desc = "Search files" })
+-- vim.keymap.set("n", "<leader>sg", "<cmd>Pick grep<CR>", { desc = "Grep files" })
+
+vim.pack.add({
+  "https://github.com/akinsho/bufferline.nvim",
+  "https://github.com/folke/which-key.nvim",
+  "https://github.com/folke/todo-comments.nvim",
+
+  "https://github.com/mcchrish/zenbones.nvim", -- colorscheme
+  "https://github.com/oskarnurm/koda.nvim", -- colorscheme
+})
+
+require("bufferline").setup()
+require("todo-comments").setup()
+
+-- require("koda").setup({ transparent = true })
+vim.cmd("colorscheme zenbones")
+
+-- bufferline
+-- TODO: Add keybinds for cycling buffers
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete current buffer" })
+vim.keymap.set("n", "<leader>bo", "<cmd>BufferLineCloseOthers<CR>", { desc = "Delete other buffers" })
+vim.keymap.set("n", "<C-b>", "<cmd>BufferLineCycleNext<CR>", { desc = "Cycle next buffer" })
+
+vim.pack.add({
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    version = "master",
+  },
+})
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "lua",
+    "python",
+  },
+  auto_install = false,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+  },
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  desc = "Handle nvim-treesitter updates",
+  group = vim.api.nvim_create_augroup("nvim-treesitter-pack-changed-update-handler", { clear = true }),
+  callback = function(event)
+    if event.data.kind == "update" and event.data.spec.name == "nvim-treesitter" then
+      vim.notify("nvim-treesitter updated, running TSUpdate...", vim.log.levels.INFO)
+      ---@diagnostic disable-next-line: param-type-mismatch
+      local ok = pcall(vim.cmd, "TSUpdate")
+      if ok then
+        vim.notify("TSUpdate completed successfully!", vim.log.levels.INFO)
+      else
+        vim.notify("TSUpdate command not available yet, skipping", vim.log.levels.WARN)
+      end
+    end
+  end,
+})
+
+vim.pack.add({
+  "https://github.com/neovim/nvim-lspconfig",
+})
+
+vim.pack.add({
+  "https://github.com/stevearc/conform.nvim",
+})
+
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- You can customize some of the format options for the filetype (:help conform.format)
+    -- rust = { "rustfmt", lsp_format = "fallback" },
+    -- Conform will run the first available formatter
+    -- javascript = { "prettierd", "prettier", stop_after_first = true },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+})
+
+vim.pack.add({
+  "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
+  "https://github.com/nvim-telescope/telescope.nvim",
+  "https://github.com/ibhagwan/fzf-lua",
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Telescope find files" })
+
+vim.cmd.filetype("plugin indent on")
--- dot_config/nvim-pure/lua/config/globals.lua
+++
@@ -1,9 +0,0 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-
-vim.cmd.filetype("plugin indent on")
-
-local g = vim.g
-g.mapleader = " "
-g.maplocalleader = " "
