6ea849d1041e0d4a9bf2fe66974f058237274a0b
Aldiantoro Nugroho <kriwil@gmail.com>
2026-03-28 21:50:27 +0700

    jubilee: nvim

---
+++ dot_config/nvim/lua/plugins/core.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = { "rktjmp/lush.nvim", lazy = false, priority = 1000 },
+  },
+  {
+    "tinted-theming/tinted-vim",
+  },
+  {
+    "oskarnurm/koda.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "zenbones",
+    },
+  },
+}
